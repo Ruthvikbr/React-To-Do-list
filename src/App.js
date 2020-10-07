4ab4f74d34ddaf9ca3466b78fb6c57a580045b59
Ruthvikbr <ruthvikbr24@gmail.com>
2020-10-07 12:21:54 +0530

    add todo list component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
+import Todos from "./Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "Learn React" },
+      { id: 2, content: "Learn React Native" },
+    ],
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos,
+    });
+  };
 
   render() {
     return (
-      <div className="App">
-        <h1>React Sample</h1>
-        <p>Welcome</p>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo List</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
